b0e11465f44974e2389efb4677f6b5e02839baaf
Evan Yang <evanyang@pop-os.localdomain>
2019-10-14 18:37:59 -0700

    added some backend + mongodb functionality but still need to fix errors with database code

---
+++ src/server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const MONGO_URL = 'mongodb://localhost/test'
+
+const app = express()
+
+mongoose.connect(MONGO_URL, {useNewUrlParser: true})
+  .then(() => {
+    console.log("DB connected")
+  }).catch(error => console.log(error))
+
+const { User } = require('./Model/user')
+
+app.use(bodyParser.json()) // converts body data to json
+
+const port = /*process.ENV.port ||*/ 4000
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+// post user data to db
+app.post('/api/user/signup', (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  }).save((err, response) => { // save: take a JSON object & stores in database
+    if (err) res.status(400).send(err)
+    res.status(200).send(response)
+  })
+})
